ce9a02ab0fe02d5c1285c8aaf1a6a6b26b357832
Alysson M. Costa <alysson@gmail.com>
2023-05-24 17:44:02 +1000

    updated slides, toyExample.

--- code/toyExampleInteger.py
+++ code/toyExampleInteger.py
@@ -1,7 +1,8 @@
+# !pip install gurobipy
 import gurobipy as gp
 from gurobipy import GRB
 
-m = gp.Model("A Linear Programming model")
+m = gp.Model("A mixed integer program")
 
 
 #variables
@@ -14,8 +15,8 @@ y = m.addVars(indices, name = 'y', vtype = GRB.BINARY)
 m.setObjective( x[1] - x[2] + y[1] + y[2] , GRB.MAXIMIZE)
 
 #constraints
-cons1 = m.addConstr( -x[1] - x[2] - y[2]   >= -2)
-cons2 = m.addConstr( x[1] + x[2] - y[1]   >= 1)
+cons1 = m.addConstr( x[1] + x[2] + y[2] <= 2)
+cons2 = m.addConstr( -x[1] - x[2] + y[1] <= -1)
 
 #solve
 m.optimize()
